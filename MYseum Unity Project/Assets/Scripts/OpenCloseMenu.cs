3d391a65a93a94cbeb65335cc1c8e078b1997dce
Jakob K. Pedersen <37065647+BaconToast420@users.noreply.github.com>
2023-11-08 13:34:02 +0100

    Spawn menu til at spawn alle 10 malerier

---
+++ MYseum Unity Project/Assets/Scripts/OpenCloseMenu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenCloseMenu : MonoBehaviour
+{
+    bool menuOpened = false;
+    public GameObject menu;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if(menuOpened == true)
+            {
+                menu.SetActive(false);
+                menuOpened = false;
+            }else if(menuOpened == false)
+            {
+                menu.SetActive(true);
+                menuOpened = true;
+            }
+        }
+    }
+}
--- MYseum Unity Project/Assets/Scripts/SpawnMaleri.cs
+++ MYseum Unity Project/Assets/Scripts/SpawnMaleri.cs
@@ -4,22 +4,11 @@ using UnityEngine;
 
 public class SpawnMaleri : MonoBehaviour
 {
-    public GameObject MonaLisa;
+    public GameObject MonaLisa, Babel, MonetBridge, Sunset, Wanderer, PearlEarring, Umbrella, AlmondBlossoms, Restaurant, StarryNight;
     public Vector3 spawnLocation;
-    bool isMonaLisaSpawned;
+    bool isMonaLisaSpawned, isBabelSpawned, isMonetBridgeSpawned, isSunsetSpawned, isWandererSpawned, isPearlEarringSpawned, isUmbrellaSpawned, isAlmondBlossomsSpawned, isRestaurantSpawned, isStarryNightSpawned;
 
-    void Start()
-    {
-        isMonaLisaSpawned = false;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    public void spawnMaleri()
+    public void spawnMonaLisa()
     {
         if(isMonaLisaSpawned == false)
             {
@@ -27,5 +16,95 @@ public class SpawnMaleri : MonoBehaviour
                 monaLisaClone.transform.Rotate(new Vector3(0, 0, 40));
                 isMonaLisaSpawned = true;
             }
+    }
+
+    public void spawnBabel()
+    {
+        if (isBabelSpawned == false)
+        {
+            GameObject babelClone = Instantiate(Babel, spawnLocation, Quaternion.identity);
+            babelClone.transform.Rotate(new Vector3(0, 0, 40));
+            isBabelSpawned = true;
+        }
+    }
+
+    public void spawnMonetBridge()
+    {
+        if (isMonetBridgeSpawned == false)
+        {
+            GameObject monetBridgeClone = Instantiate(MonetBridge, spawnLocation, Quaternion.identity);
+            monetBridgeClone.transform.Rotate(new Vector3(0, 0, 40));
+            isMonetBridgeSpawned = true;
+        }
+    }
+
+    public void spawnSunset()
+    {
+        if (isSunsetSpawned == false)
+        {
+            GameObject sunsetClone = Instantiate(Sunset, spawnLocation, Quaternion.identity);
+            sunsetClone.transform.Rotate(new Vector3(0, 0, 40));
+            isSunsetSpawned = true;
+        }
+    }
+
+    public void spawnWanderer()
+    {
+        if (isWandererSpawned == false)
+        {
+            GameObject wandererClone = Instantiate(Wanderer, spawnLocation, Quaternion.identity);
+            wandererClone.transform.Rotate(new Vector3(0, 0, 40));
+            isWandererSpawned = true;
+        }
+    }
+
+    public void spawnPearlEarring()
+    {
+        if (isPearlEarringSpawned == false)
+        {
+            GameObject PearlEarringClone = Instantiate(PearlEarring, spawnLocation, Quaternion.identity);
+            PearlEarringClone.transform.Rotate(new Vector3(0, 0, 40));
+            isPearlEarringSpawned = true;
+        }
+    }
+
+    public void spawnUmbrella()
+    {
+        if (isUmbrellaSpawned == false)
+        {
+            GameObject clone = Instantiate(Umbrella, spawnLocation, Quaternion.identity);
+            clone.transform.Rotate(new Vector3(0, 0, 40));
+            isUmbrellaSpawned = true;
+        }
+    }
+
+    public void spawnAlmondBlossoms()
+    {
+        if (isAlmondBlossomsSpawned == false)
+        {
+            GameObject clone = Instantiate(AlmondBlossoms, spawnLocation, Quaternion.identity);
+            clone.transform.Rotate(new Vector3(0, 0, 40));
+            isAlmondBlossomsSpawned = true;
+        }
+    }
+
+    public void spawnRestaurant()
+    {
+        if (isRestaurantSpawned == false)
+        {
+            GameObject clone = Instantiate(Restaurant, spawnLocation, Quaternion.identity);
+            clone.transform.Rotate(new Vector3(0, 0, 40));
+            isRestaurantSpawned = true;
+        }
+    }
+
+    public void spawnStarryNight()
+    {
+        if (isStarryNightSpawned == false)
+        {
+            GameObject clone = Instantiate(StarryNight, spawnLocation, Quaternion.identity);
+            clone.transform.Rotate(new Vector3(0, 0, 40));
+            isStarryNightSpawned = true;
+        }
     }
 }
